514a9445900e421fc9d3f82065384c77ea7d408c
Regy Saputra <45072445+regysaputra@users.noreply.github.com>
2022-11-24 16:32:10 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <p>Hello, World</p>
+      </div>
+    );
+  }
+}
+
+export default App;
